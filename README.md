6e7b8078741842ea68487640c4014df119701046
lenschcode <danlennardhaensch@gmail.com>
2023-10-26 19:43:24 +0200

    Init, Midi Executor mapping working for Buttons and Faders

---
+++ README.md
@@ -0,0 +1,10 @@
+# MA3 Midi Executors
+Allows you to map Midi Remotes in MA3 to Executors
+
+
+-------- Tutorial -------------
+-- In order to map a Midi Remote to an executor create a midi remote with your Midi mapping and simply change the name to 
+-- "* Executor <101> <Key/Fader>"
+-- where <101> can be any executor number and the Name can have any prefix
+-- examples: "APC40 Fader1 Executor 201 Fader", "Launchpad Button 1 Executor 101 Key"
+-- The Midi remotes are always mapped to the current page
---
+++ midi_executors_main.lua
@@ -0,0 +1,137 @@
+-- ********************************************************
+-- **************** Midi Executors v1.0.0 *****************
+-- ******************** by LenschCode *********************
+-- ********************************************************
+-- ******* Allows to map Midi Remotes to Executors ********
+-- ********************************************************
+
+-- ***************** Configuration Guide ******************
+-- *** https://github.com/lenschcode/MA3-Midi-Executors ***
+-- ********************************************************
+
+-----------------------------------------------------------
+----------------------- Refreshrate -----------------------
+-- Determines how often the Midi assignment gets updated --
+------------ Rate in seconds, 0.5 Recommended -------------
+------------- Note: Incoming midi is instant --------------
+                    local rate = 0.5
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+----------------------- Program Code ----------------------
+---------------- Don't change anything here ---------------
+-----------------------------------------------------------
+------ Basically checks the name of Midi Remotes for ------
+--- Executor information and then assigns the Executors ---
+--- Target and Key and Fader Actions to the Midi Remote ---
+-----------------------------------------------------------
+
+
+local running = false
+
+local function checkExecutor(name)
+    local exec = {}
+    exec.page = CurrentExecPage().no
+    exec.id, exec.type = name:match("Executor (%d+) (%a+)")
+
+    if exec.page and exec.id and exec.type == "Key" or exec.type == "Fader" then
+        return exec
+    else
+        return nil
+    end
+end
+
+local function getExecKey(page, id)
+    local exec = ObjectList("page " .. page .. "." .. id)[1]
+
+    if exec ~= nil then
+        return exec.key
+    else
+        return nil
+    end
+end
+
+local function getExecFader(page, id)
+    local exec = ObjectList("page " .. page .. "." .. id)[1]
+
+    if exec ~= nil then
+        return exec.fader
+    else
+        return nil
+    end
+
+end
+
+local function getExecObject(page, id)
+    local exec = ObjectList("page " .. page .. "." .. id)[1]
+
+    if exec ~= nil then
+        return exec.object
+    else
+        return nil
+    end
+end
+
+local function parseMidiRemotes()
+    for key, remote in pairs(Root().ShowData.Remotes.MIDIRemotes:Children()) do
+        local exec = checkExecutor(remote.name)
+        if exec ~= nil then
+            remote.target = getExecObject(exec.page, exec.id)
+            if remote.target == nil then
+                remote.key = ""
+                remote.fader = ""
+            else
+                if exec.type == "Key" then
+                    remote.key = getExecKey(exec.page, exec.id)
+                    remote.fader = ""
+                else
+                    remote.key = ""
+                    remote.fader = getExecFader(exec.page, exec.id)
+                end
+            end
+        end
+    end
+end
+
+local function loop()
+    while running do
+        parseMidiRemotes()
+        coroutine.yield(rate)
+    end
+end
+
+local function StartGui(config)
+	local descTable = {
+		title = "Midi Executors",
+		caller = GetFocusDisplay(),
+		items = {"Start","Stop","Run Once"},
+		selectedValue = "Start",
+		add_args = {FilterSupport="Yes"},
+	}
+	if running then
+		descTable.items = {"Stop"}
+	else
+		descTable.items = {"Start","Run Once"}
+	end
+
+	local index, name = PopupInput(descTable)
+    return index, name;
+end
+
+function main()
+    local index, name = StartGui()
+
+    if name == "Start" then
+        if not running then
+            running = true
+            loop()
+        end
+    elseif name == "Stop" then
+        running = false
+    elseif name == "Run Once" then
+        parseMidiRemotes()
+    end
+
+end
+
+return main
